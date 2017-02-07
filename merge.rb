0765a60f38829fa10abbc0ad718d3226a9855bbf
ryanthomas92 <rthoma11@villanova.edu>
2017-02-06 16:31:42 -0800

    merge files to repo

--- merge.rb
+++ merge.rb
@@ -1,3 +1,38 @@
 def merge(arr1, arr2)
-  # code in here
+  #check if there are any non numbers in either array
+    return nil if !(arr1.all? { |num| num.is_a? Numeric })
+    return nil if !(arr2.all? { |num| num.is_a? Numeric })
+
+  # set up a results array
+  sorted_arr = []
+  #measure the length of each array
+  len1 = arr1.length
+  len2 = arr2.length
+  #set up a counter for each array
+  i = 0
+  j = 0
+
+  #loop stops when one array is checked fully
+  while i < len1 && j < len2
+    #compare values of array 1 and array 2
+    if arr1[i] <= arr2[j]
+      #if value in array 1 is less, push it
+      sorted_arr << arr1[i]
+      #increment counter
+      i += 1
+    else
+      sorted_arr << arr2[j]
+      j += 1
+    end
+  end
+
+  #are there are remaining items in either array?
+  #if so push the remaining item of one into final sorted array
+  if i < len1
+    sorted_arr += arr1[i..-1]
+  else
+    sorted_arr += arr2[j..-1]
+  end
+  # implicit return
+  sorted_arr
 end
